2a8f204e25da8a45d5110446edc848365389de9e
Rey <mikaelarey025@gmail.com>
2021-01-10 05:43:03 -0800

    SQL Server Simple CRUD Operation

---
+++ CSharp/SQLServerCrud/SQLServerCrud/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SQLServerCrud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.Edit = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(16, 51);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(152, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(16, 106);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(152, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(16, 142);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(152, 23);
+            this.Add.TabIndex = 4;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Edit
+            // 
+            this.Edit.Location = new System.Drawing.Point(16, 171);
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(152, 23);
+            this.Edit.TabIndex = 5;
+            this.Edit.Text = "Edit";
+            this.Edit.UseVisualStyleBackColor = true;
+            this.Edit.Click += new System.EventHandler(this.Edit_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(16, 200);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(152, 23);
+            this.Delete.TabIndex = 6;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(189, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(427, 189);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChange);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 245);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Edit);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Edit;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CSharp/SQLServerCrud/SQLServerCrud/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQLServerCrud
+{
+    public partial class Form1 : Form
+    {
+        //public string server = @"DESKTOP-VPUITR2\SQLEXPRESS";
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-VPUITR2\SQLEXPRESS;Initial Catalog=Sample;Integrated Security=true;");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+
+        private int UserId;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Set datagridview property to readonly to prevent from editing cells
+            // and selection mode to fullrowselect to select the full row on cell click.
+
+            dataGridView1.MultiSelect = false;  // allow 1 row only to be selected
+            
+            GetAllUsers();
+            ClearData();
+
+            dataGridView1.ClearSelection();     // clear the default selection on form load
+        }
+
+        private void GetAllUsers() 
+        {
+            try
+            {
+                con.Open();
+                DataTable dt = new DataTable();
+                adapt = new SqlDataAdapter("Select * from users", con);
+                adapt.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ClearData()
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsername.Text != "" && txtPassword.Text != "")
+                {
+                    cmd = new SqlCommand("insert into users(Username, Password) values (@username, @password)", con);
+                    con.Open();
+
+                    cmd.Parameters.AddWithValue("@username", txtUsername.Text);
+                    cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    MessageBox.Show("Record Inserted Successfully");
+
+                    GetAllUsers();
+                    ClearData();
+                }
+                else
+                {
+                    MessageBox.Show("Please provide data for both username and password");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void GetSelectedData()
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                UserId = Convert.ToInt32(row.Cells[0].Value.ToString());
+                txtUsername.Text = row.Cells[1].Value.ToString();
+                txtPassword.Text = row.Cells[2].Value.ToString();
+            }
+        }
+
+        private void Edit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsername.Text != "" && txtPassword.Text != "")
+                {
+                    cmd = new SqlCommand("update users set Username=@username, Password=@password where UserId=" + UserId, con);
+                    con.Open();
+
+                    cmd.Parameters.AddWithValue("@username", txtUsername.Text);
+                    cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    MessageBox.Show("Record Updated Successfully");
+
+                    GetAllUsers();
+                    ClearData();
+                }
+                else
+                {
+                    MessageBox.Show("Please provide data for both username and password");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_SelectionChange(object sender, EventArgs e)
+        {
+            GetSelectedData();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                    cmd = new SqlCommand("delete from users where UserId=" + UserId, con);
+                    con.Open();
+
+                    cmd.Parameters.AddWithValue("@username", txtUsername.Text);
+                    cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    MessageBox.Show("Record Deleted Successfully");
+
+                    GetAllUsers();
+                    ClearData();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
